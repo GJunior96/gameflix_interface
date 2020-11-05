a77f0ce9967386f3f63d27f620dfad3b49f07d37
kenobi <kenobi@pop-os.localdomain>
2020-11-05 15:37:49 -0300

    Menu and Intro (Desktop and Mobile)

--- js/script.js
+++ js/script.js
@@ -4,11 +4,6 @@ var btnSearch = document.querySelector('.btn-search');
 btnSearch.addEventListener('click', showBoxSearch);
 
 function showBoxSearch() {
-    // boxSearch.style.setProperty('display', 'inline-block');
-    // boxSearch.style.setProperty('width', '120px');
-    // boxSearch.style.setProperty('position', 'absolute');
-    // boxSearch.style.setProperty('top', '19px');
-    // boxSearch.style.setProperty('right', '150px');
     boxSearch.classList.toggle('active');
     btnSearch.classList.toggle('active');
     boxSearch.focus();
